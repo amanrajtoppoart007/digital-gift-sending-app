1b117c12534330931f36b5dffa75cbf479c73794
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-17 21:17:03 +0530

    migration error fixed

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\State;
 use App\Models\User;
+use Faker\Provider\Address;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -16,11 +18,14 @@ class UsersTableSeeder extends Seeder
                 'email'              => 'moderator@krishakvikas.com',
                 'password'           => bcrypt('password'),
                 'remember_token'     => null,
+                 'state_id'           => (State::inRandomOrder()->first())->id,
                 'approved'           => 1,
                 'verified'           => 1,
                 'verified_at'        => '2021-01-26 15:07:08',
                 'verification_token' => '',
                 'mobile'             => '1234567890',
+                'city'=>'',
+                'address'=>Address::asciify()
             ],
         ];
 
