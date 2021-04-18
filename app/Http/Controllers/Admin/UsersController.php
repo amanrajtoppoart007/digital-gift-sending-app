a877caa46a39af43fa9752c216aa9a469f672a7d
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-18 12:52:47 +0530

    Update UsersController.php

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -127,7 +127,7 @@ class UsersController extends Controller
     public function edit(User $user)
     {
         abort_if(Gate::denies('user_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-         $states = State::all();
+         $states = State::all()->pluck('name', 'id')->prepend(trans('global.pleaseSelect'), '');
         return view('admin.users.edit', compact('user','states'));
     }
 
