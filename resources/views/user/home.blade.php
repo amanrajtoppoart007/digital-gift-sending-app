9de8ac34a85ee4dd195c4c9bc2c6e689edf2121d
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-20 12:58:56 +0530

    Update home.blade.php

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -8,12 +8,100 @@
                     Dashboard
                 </div>
 
-                <div class="card-body" style="min-height: 450px">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <div class="card-body" style="min-height:450px">
+
+               <div class="row mt-3">
+
+                <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                    <div class="card border-0 shadow">
+                        <a class="card-body">
+                            <br>
+                            <img src="http://127.0.0.1:8000/front-assets/images/account.svg" alt="img-1" class="img-fluid how-work-img">
+                            <h3 class="font-weight-bold mt-3 text-theme-1">Create Template</h3>
+                            <hr class="w-50">
+                            <p class="text-secondary">Create your blessing page to get started.</p>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <br>
+                            <img src="{{asset('front-assets/images/create.svg')}}" alt="img-2" class="img-fluid how-work-img">
+                            <h3 class="font-weight-bold mt-3 text-theme-1">Add Account</h3>
+                            <hr class="w-50">
+                            <p class="text-secondary">Add your bank account detail to get payment directly in your bank account.</p>
                         </div>
-                    @endif
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <br>
+                            <img src="{{asset('front-assets/images/link.svg')}}" alt="img-3" class="img-fluid how-work-img">
+                            <h3 class="font-weight-bold mt-3 text-theme-1">Share Profile</h3>
+                            <hr class="w-50">
+                            <p class="text-secondary">Share your profile with people to receive blessings.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12 p-3">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <br>
+                            <img src="{{asset('front-assets/images/delivered.svg')}}" alt="img-4" class="img-fluid how-work-img">
+                            <h3 class="font-weight-bold mt-3 text-theme-1">Received Amount</h3>
+                            <hr class="w-50">
+                            <p class="text-secondary">Payment history of received blessings.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+                        <h6><strong>User Detail</strong></h6>
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>{{auth()->user()->name}}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>{{auth()->user()->email}}</td>
+                        </tr>
+                         <tr>
+                            <th>Mobile</th>
+                            <td>{{auth()->user()->mobile}}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                        <h6><strong>Account Detail</strong></h6>
+                    <table class="table">
+                        <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Bank Name</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Account Number</th>
+                            <td></td>
+                        </tr>
+                         <tr>
+                            <th>IFSC Code</th>
+                            <td></td>
+                        </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
