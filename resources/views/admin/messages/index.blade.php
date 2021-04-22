8a3de62e27575d9382617bd01b459cfd048e6df3
Homesh Kumar Verma <iamhomesh@Gmail.com>
2021-04-22 18:57:34 +0530

    Message CRUD

---
+++ resources/views/admin/messages/index.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.admin')
+@section('content')
+    @can('user_alert_create')
+        <div style="margin-bottom: 10px;" class="row">
+            <div class="col-lg-12">
+                <button class="btn btn-success" id="addButton">
+                    {{ trans('global.add') }} Message
+                </button>
+            </div>
+        </div>
+    @endcan
+    <div class="card">
+        <div class="card-header">
+            Message {{ trans('global.list') }}
+        </div>
+
+        <div class="card-body">
+            <table class=" table table-bordered table-striped table-hover ajaxTable datatable datatable-UserAlert">
+                <thead>
+                <tr>
+                    <th width="10">
+
+                    </th>
+                    <th>
+                        {{ trans('cruds.userAlert.fields.id') }}
+                    </th>
+                    <th>
+                        Type
+                    </th>
+                    <th>
+                        Message
+                    </th>
+                    <th>
+                        &nbsp;
+                    </th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+
+    <div class="modal fade" id="addModal" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Add/Edit User Alert</h4>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">×</span></button>
+                </div>
+                <form id="userAlertForm">
+                    <input type="hidden" name="id" id="id">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label class="required"
+                                   for="type">type</label>
+                            <input class="form-control" type="text"
+                                   name="type" id="type" required>
+                        </div>
+                        <div class="form-group">
+                            <label class="required"
+                                   for="message">type</label>
+                            <input class="form-control" type="text"
+                                   name="message" id="message" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
+                        <button class="btn btn-danger" type="submit">{{ trans('global.save') }}</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
+@section('scripts')
+    @parent
+    <script>
+        $(function () {
+            let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+            
+            let dtOverrideGlobals = {
+                buttons: dtButtons,
+                processing: true,
+                serverSide: true,
+                retrieve: true,
+                aaSorting: [],
+                ajax: "{{ route('admin.messages.index') }}",
+                columns: [
+                    {data: 'placeholder', name: 'placeholder'},
+                    {data: 'id', name: 'id'},
+                    {data: 'type', name: 'type'},
+                    {data: 'message', name: 'message'},
+                    {data: 'actions', name: '{{ trans('global.actions') }}'},
+                ],
+                orderCellsTop: true,
+                order: [[1, 'desc']],
+                pageLength: 100,
+            };
+            let table = $('.datatable-UserAlert').DataTable(dtOverrideGlobals);
+            $('a[data-toggle="tab"]').on('shown.bs.tab click', function (e) {
+                $($.fn.dataTable.tables(true)).DataTable()
+                    .columns.adjust();
+            });
+
+            jQuery.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            let isUpdate = false;
+            $('#addButton').click(() => {
+                isUpdate == false;
+                $('#addModal').modal('show');
+            });
+            $('#userAlertForm').submit(function (event) {
+                event.preventDefault();
+                let url = isUpdate ? "{{ route('admin.messages.update') }}" : "{{ route("admin.messages.store") }}";
+                $.post(url, $(this).serialize(), result => {
+                    alert(result.msg)
+                    if (result.status) {
+                        location.reload()
+                    }
+                }, 'json');
+            });
+
+            $(document).on('click','.editButton', function (){
+                let id = $(this).data('id');
+                isUpdate = true;
+                $.get("{{ route('admin.messages.edit') }}", {id}, result => {
+                    if (result.status) {
+                        $('#type').val(result.data.type);
+                        $('#message').val(result.data.message);
+                        $('#id').val(result.data.id);
+                        $('#addModal').modal('show');
+                    }
+                }, 'json');
+            });
+
+            $('.editButton').click(function () {
+
+            });
+
+        });
+
+    </script>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,42 +1,46 @@
 <?php
-    Route::get('/', 'Admin\HomeController@index')->name('home');
-    Route::get('/dashboard', 'Admin\HomeController@index')->name('dashboard');
-    // Permissions
-    Route::delete('permissions/destroy', 'Admin\PermissionsController@massDestroy')->name('permissions.massDestroy');
-    Route::resource('permissions', 'Admin\PermissionsController');
-    // Roles
-    Route::delete('roles/destroy', 'Admin\RolesController@massDestroy')->name('roles.massDestroy');
-    Route::resource('roles', 'Admin\RolesController');
-    // Users
-    Route::delete('users/destroy', 'Admin\UsersController@massDestroy')->name('users.massDestroy');
-    Route::post('users/parse-csv-import', 'Admin\UsersController@parseCsvImport')->name('users.parseCsvImport');
-    Route::post('users/process-csv-import', 'Admin\UsersController@processCsvImport')->name('users.processCsvImport');
-    Route::resource('users', 'Admin\UsersController');
-    Route::post('users/add-profile/{$user}', [\App\Http\Controllers\Admin\UsersController::class, 'addProfile'])->name('users.add-profile');
-    // User Alerts
-    Route::delete('user-alerts/destroy', 'Admin\UserAlertsController@massDestroy')->name('user-alerts.massDestroy');
-    Route::resource('user-alerts', 'Admin\UserAlertsController', ['except' => ['edit', 'update']]);
-    Route::get('get-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'getUserAlert'])->name('get.user-alert');
-    Route::post('add-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'addUserAlert'])->name('user-alert.add');
-    Route::post('update-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'updateUserAlert'])->name('user-alert.update');
-    // Audit Logs
-    Route::resource('audit-logs', 'Admin\AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
-    // Settings
-    Route::delete('settings/destroy', 'Admin\SettingsController@massDestroy')->name('settings.massDestroy');
-    Route::post('settings/parse-csv-import', 'Admin\SettingsController@parseCsvImport')->name('settings.parseCsvImport');
-    Route::post('settings/process-csv-import', 'Admin\SettingsController@processCsvImport')->name('settings.processCsvImport');
-    Route::resource('settings', 'Admin\SettingsController');
-    // Admins
-    Route::delete('admins/destroy', 'Admin\AdminController@massDestroy')->name('admins.massDestroy');
-    Route::post('admins/parse-csv-import', 'Admin\AdminController@parseCsvImport')->name('admins.parseCsvImport');
-    Route::post('admins/process-csv-import', 'Admin\AdminController@processCsvImport')->name('admins.processCsvImport');
-    Route::resource('admins', 'Admin\AdminController');
+Route::get('/', 'Admin\HomeController@index')->name('home');
+Route::get('/dashboard', 'Admin\HomeController@index')->name('dashboard');
+// Permissions
+Route::delete('permissions/destroy', 'Admin\PermissionsController@massDestroy')->name('permissions.massDestroy');
+Route::resource('permissions', 'Admin\PermissionsController');
+// Roles
+Route::delete('roles/destroy', 'Admin\RolesController@massDestroy')->name('roles.massDestroy');
+Route::resource('roles', 'Admin\RolesController');
+// Users
+Route::delete('users/destroy', 'Admin\UsersController@massDestroy')->name('users.massDestroy');
+Route::post('users/parse-csv-import', 'Admin\UsersController@parseCsvImport')->name('users.parseCsvImport');
+Route::post('users/process-csv-import', 'Admin\UsersController@processCsvImport')->name('users.processCsvImport');
+Route::resource('users', 'Admin\UsersController');
+Route::post('users/add-profile/{$user}', [\App\Http\Controllers\Admin\UsersController::class, 'addProfile'])->name('users.add-profile');
+// User Alerts
+Route::delete('user-alerts/destroy', 'Admin\UserAlertsController@massDestroy')->name('user-alerts.massDestroy');
+Route::resource('user-alerts', 'Admin\UserAlertsController', ['except' => ['edit', 'update']]);
+Route::get('get-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'getUserAlert'])->name('get.user-alert');
+Route::post('add-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'addUserAlert'])->name('user-alert.add');
+Route::post('update-user-alert', [\App\Http\Controllers\Admin\UserAlertsController::class, 'updateUserAlert'])->name('user-alert.update');
+// Audit Logs
+Route::resource('audit-logs', 'Admin\AuditLogsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+// Settings
+Route::delete('settings/destroy', 'Admin\SettingsController@massDestroy')->name('settings.massDestroy');
+Route::post('settings/parse-csv-import', 'Admin\SettingsController@parseCsvImport')->name('settings.parseCsvImport');
+Route::post('settings/process-csv-import', 'Admin\SettingsController@processCsvImport')->name('settings.processCsvImport');
+Route::resource('settings', 'Admin\SettingsController');
+// Admins
+Route::delete('admins/destroy', 'Admin\AdminController@massDestroy')->name('admins.massDestroy');
+Route::post('admins/parse-csv-import', 'Admin\AdminController@parseCsvImport')->name('admins.parseCsvImport');
+Route::post('admins/process-csv-import', 'Admin\AdminController@processCsvImport')->name('admins.processCsvImport');
+Route::resource('admins', 'Admin\AdminController');
 
-    Route::group(['prefix' => 'template', 'as' => 'template.'], function () {
-Route::get('create/{userId}', 'Admin\TemplateController@create')->name('create');
-Route::get('edit/{id}', 'Admin\TemplateController@edit')->name('edit');
-Route::post('store', 'Admin\TemplateController@store')->name('store');
-Route::post('update/{id}', 'Admin\TemplateController@update')->name('update');
-Route::get('show/{username}', 'Admin\TemplateController@show')->name('show');
-Route::any('destroy/{id}', 'Admin\TemplateController@destroy')->name('delete');
+Route::group(['prefix' => 'template', 'as' => 'template.'], function () {
+    Route::get('create/{userId}', 'Admin\TemplateController@create')->name('create');
+    Route::get('edit/{id}', 'Admin\TemplateController@edit')->name('edit');
+    Route::post('store', 'Admin\TemplateController@store')->name('store');
+    Route::post('update/{id}', 'Admin\TemplateController@update')->name('update');
+    Route::get('show/{username}', 'Admin\TemplateController@show')->name('show');
+    Route::any('destroy/{id}', 'Admin\TemplateController@destroy')->name('delete');
 });
+Route::get('messages', 'Admin\MessageController@index')->name('messages.index');
+Route::post('messages/store', 'Admin\MessageController@store')->name('messages.store');
+Route::get('messages/edit', 'Admin\MessageController@edit')->name('messages.edit');
+Route::post('messages/update', 'Admin\MessageController@update')->name('messages.update');
