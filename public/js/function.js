b3de0c176085a74beed3b1c9a5b13ff51cd6e1c1
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-24 04:36:56 +0530

    unnecessary js files removed

--- public/js/function.js
+++ public/js/function.js
@@ -1,26 +1,27 @@
 (function () {
     $.notify = function (text, type, position = null) {
-        let color;
+        let bgColor;
+
         switch (type) {
             case "success":
-                color = "#219310";
+                bgColor = "#219310";
                 break;
             case "error":
-                color = "#9E250E";
+                bgColor = "#9E250E";
                 break;
             case "warning":
-                color = "#FA830A";
+                bgColor = "#FA830A";
                 break;
             default:
-                color = "#1E27FF";
+                bgColor = "#1E27FF";
                 break;
         }
 
         $.toast({
             text: text,
             showHideTransition: 'slide',  // It can be plain, fade or slide
-            bgColor: '#A9A9A9',              // Background color for toast
-            textColor: color,            // text color
+            bgColor: bgColor,              // Background color for toast
+            textColor: '#ffffff',            // text color
             allowToastClose: false,       // Show the close button or not
             hideAfter: 5000,              // `false` to make it sticky or time in milliseconds to hide after
             stack: 5,                     // `false` to show one stack at a time count showing the number of toasts that can be shown at once
