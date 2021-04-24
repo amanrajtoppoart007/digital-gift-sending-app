adb12b108e5287918bc776754e935f6a40bdb536
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-24 15:39:19 +0530

    Update navbar.blade.php

--- resources/views/user/includes/navbar.blade.php
+++ resources/views/user/includes/navbar.blade.php
@@ -1,7 +1,6 @@
 <!-- Header (Start) -->
 <header id="header">
     <nav class="navbar bg-theme-1 navbar-dark  navbar-expand-md shadow-sm">
-        <div class="container py-3">
             <a href="{{route('home')}}" class="navbar-brand logo font-weight-bold">{{ trans('panel.site_title') }}</a>
             <div class="collapse navbar-collapse" id="navbar-menu">
                 <ul class="navbar-nav ml-auto">
@@ -25,7 +24,6 @@
             <button class="navbar-toggler" id="navbar-toggler">
                 <img src="{{ asset('front-assets/images/menu.svg') }}" alt="navbar-menu">
             </button>
-        </div>
     </nav>
 </header>
 <!-- Header (End) -->
