4844a16d382d34f6ce9748da8af38b659dd220c9
amanrajtoppoart007 <42941876+amanrajtoppoart007@users.noreply.github.com>
2021-04-26 01:15:11 +0530

    Update cmd.php

--- routes/cmd.php
+++ routes/cmd.php
@@ -2,7 +2,20 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/clear-db', function () {
+Route::get('/db-clear', function () {
     Artisan::call('db:wipe');
+    dd("done");
+});
+
+Route::get('/config-clear', function () {
+    Artisan::call('config:clear');
+    dd("done");
+});
+Route::get('/view-clear', function () {
+    Artisan::call('view:clear');
+    dd("done");
+});
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
     dd("done");
 });
